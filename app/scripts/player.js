dbcbf2fdcd7fb68588fec5dbf97616210a367a2f
danben4 <danielbenediktsson4@gmail.com>
2015-03-28 13:51:07 +0000

    Player moving in y-direction when clicking space

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -26,6 +26,12 @@ window.Player = (function() {
 	};
 
 	Player.prototype.onFrame = function(delta) {
+        if(Controls.keys.space) {
+            this.pos.y -= delta * SPEED;
+        }
+        if(!Controls.keys.space) {
+            this.pos.y += delta * SPEED;
+        }
 		if (Controls.keys.right) {
 			this.pos.x += delta * SPEED;
 		}
