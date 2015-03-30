cfa5df0265556f7ec49e01f31a3f97598a81f280
danben4 <danielbenediktsson4@gmail.com>
2015-03-30 10:22:02 +0000

    pipes is getting real

--- app/scripts/pipe.js
+++ app/scripts/pipe.js
@@ -1,7 +1,6 @@
 window.Pipe = (function() {
     'use strict';
-
-    //var SPEED = 10;
+    var SPEED = 10;
     //var GAP = 25;
 
     var NewPipe = function(pipe, xcoord, ycoord) {
@@ -15,15 +14,23 @@ window.Pipe = (function() {
         this.pipes = [
             {
                 name: 'FirstPipe',
-                top: new NewPipe(this.el.find('.PipeUp1'), 10, 10),
-                bottom: new NewPipe(this.el.find('.PipeDown1'), 10, 10)
+                top: new NewPipe(this.el.find('.PipeUp1'), 0, 0),
+                bottom: new NewPipe(this.el.find('.PipeDown1'), 0, 0)
             }
         ];
     };
 
     Pipe.prototype.reset = function() {
-        this.pipes[0].top.pos.x = 10;
-        this.pipes[0].bottom.pos.x = 10;
+        this.pipes[0].top.pos.x = 0;
+        this.pipes[0].bottom.pos.x = 0;
+    };
+
+    Pipe.prototype.onFrame = function(delta) {
+        this.pipes[0].top.pos.x -= delta * SPEED;
+        this.pipes[0].bottom.pos.x -= delta * SPEED;
+
+		this.el.css('transform', 'translate(' + this.pipes[0].top.pos.x + 'em, ' + this.pipes[0].top.pos.y + 'em)');
+		this.el.css('transform', 'translate(' + this.pipes[0].bottom.pos.x + 'em, ' + this.pipes[0].bottom.pos.y + 'em)');
     };
 
     return Pipe;
