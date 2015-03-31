af5229e23a7a4afd8c460419463d9c8d60ba7d4a
danben4 <danielbenediktsson4@gmail.com>
2015-03-31 09:43:06 +0000

    flott

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -1,11 +1,12 @@
 window.Player = (function() {
 	'use strict';
 
-	var Controls = window.Controls;
+	//var Controls = window.Controls;
 
 	// All these constants are in em's, multiply by 10 pixels
 	// for 1024x576px canvas.
-	var SPEED = 10;
+	var SPEED = 0;
+    var GRAVITY = 1.0;
 	var WIDTH = 5;
 	var HEIGHT = 5;
 	var INITIAL_POSITION_X = 30;
@@ -15,6 +16,13 @@ window.Player = (function() {
 		this.el = el;
 		this.game = game;
         this.pos = { x: 0, y: 0 };
+        $(window).on('keydown', function(e) {
+            if(e.keyCode === 32) {
+                SPEED = SPEED - 40;
+                //console.log('inside keydown');
+                return true;
+            }
+        });
 	};
 
 	/**
@@ -23,19 +31,24 @@ window.Player = (function() {
 	Player.prototype.reset = function() {
 		this.pos.x = INITIAL_POSITION_X;
 		this.pos.y = INITIAL_POSITION_Y;
+	    SPEED = 0;
 		//this.el.css('transform', 'translate(' + this.pos.x + 'em, ' + this.pos.y + 'em)');
 	};
 
 	Player.prototype.onFrame = function(delta) {
+        SPEED = SPEED + GRAVITY;
+        this.pos.y += delta * SPEED;
+
+        /*
         if(Controls.keys.mousedown || Controls.keys.touchstart || Controls.keys.space) {
             //this.pos.y -= 3;
             document.getElementById('Flapp').play();
             this.pos.y -= delta * SPEED + 1.5;
         }
         else {
-            //this.pos.y += delta + 0.5;
             this.pos.y += delta * SPEED;
         }
+        */
 
 		// Update UI
 		this.el.css('transform', 'translate(' + this.pos.x + 'em, ' + this.pos.y + 'em)');
@@ -48,48 +61,26 @@ window.Player = (function() {
     Player.prototype.checkCollisionWithPipes = function() {
         var pipeWidth = 7.5;
         for(var i = 0; i < this.game.pipe.pipes.length; i++) {
+            var pipeX = this.game.pipe.pipes[i].bottom.pos.x;
+            //var height1 = parseFloat(this.game.pipe.el[2 * i].style.height);
+                
             /*
-            console.log('this.el: ', this.game.pipe.el);
-            console.log('xcoord: ', this.game.pipe.pipes[i].top.pos.x);
-            console.log('ycoord1: ', parseFloat(this.game.pipe.el[2 * i].style.height));
-            console.log('ycoord2: ', (this.game.WORLD_HEIGHT - parseFloat(this.game.pipe.el[2 * i + 1].style.height)));
-            console.log('height1: ', parseFloat(this.game.pipe.el[2 * i].style.height));
-            console.log('height2: ', parseFloat(this.game.pipe.el[2 * i + 1].style.height));
-            console.log('width: ', pipeWidth);
-            */
-            var pipeX = this.game.pipe.pipes[i].top.pos.x - pipeWidth;
-            var height1 = parseFloat(this.game.pipe.el[2 * i].style.height);
-
-            // between x-coord
-            if((pipeX > 0) &&
-                (pipeX < this.game.WORLD_WIDTH) &&
-                (pipeX < (this.pos.x + WIDTH)) &&
-                (pipeX > this.pos.x - WIDTH) &&
+            var CONST = 24.8;
+            if((-pipeX - CONST >= this.pos.x + WIDTH) &&
+                ((-pipeX - pipeWidth - CONST) <= this.pos.x + WIDTH) &&
                 (this.pos.y < height1)) {
-                console.log('gameover!!');
-                console.log('i: ', i);
-                //return this.game.gameover();
-                //console.log('first check true');
-                //console.log('i: ', i);
-                //console.log(this.game.pipe.pipes[i]);
-                //var height1 = parseFloat(this.game.pipe.el[2 * i].style.height);
-                //console.log('height1: ', height1);
-                //console.log('this.pos.y - HEIGHT: ', this.pos.y - HEIGHT);
-                //console.log(this.game.pipe.el);
-                /*
-                if(this.pos.y < height1) {
-                    console.log('second check TRUE');
-                    console.log('height1: ', height1);
-                    console.log('this.pos.y: ', this.pos.y);
-                    return this.game.gameover();
-                }
-                else {
-                    console.log('second check FALSE');
-                }
                 */
+            if((-pipeX >= this.pos.x + WIDTH) &&
+                ((-pipeX - pipeWidth) <= this.pos.x + WIDTH)) {
+                console.log('i: ', i);
+                console.log('first check true');
+                console.log('-pipeX: ', -pipeX);
+                console.log('-pipeX - pipeWidth: ', -pipeX - pipeWidth);
+                console.log('this.pos.x :', this.pos.x);
+                console.log('WIDTH :', WIDTH);
+                //return this.game.gameover();
             }
             else {
-                //console.log('i: ', i);
                 console.log('first check FALSE');
             }
 
